b581b5da21d658d60387b4ab56a69c8906e8ab4b
Ali Furkan Dadali <67693130+furkandadali@users.noreply.github.com>
2022-01-14 14:51:53 +0300

    Update SQL_Query_DB

--- SQL_QUERY_DataBase.py
+++ SQL_QUERY_DataBase.py
@@ -9,13 +9,13 @@ def queryDatabase(limitParameter):
                                       database="postgres")
         cursor = connection.cursor()
 
-        postgreSQL_insert_Query =  """INSERT INTO public."BitExenLiveDemo" ("marketcode","lastprice",dailyaverage,buyerorderprice,sellerorderprice) VALUES (%s,%s,%s,%s,%s);"""
+        postgreSQL_insert_Query =  """INSERT INTO public."DBNAME" ("marketcode","lastprice",dailyaverage,buyerorderprice,sellerorderprice) VALUES (%s,%s,%s,%s,%s);"""
 
-        sql_select_query_1 = """select lastprice as LP from public."BitExenLiveDemo" ORDER BY Id DESC LIMIT %s  """
+        sql_select_query_1 = """select lastprice as LP from public."DBNAME" ORDER BY Id DESC LIMIT %s  """
 
-        sql_select_query_2 = """select dailyaverage as DA from public."BitExenLiveDemo" ORDER BY Id DESC LIMIT %s  """
+        sql_select_query_2 = """select dailyaverage as DA from public."DBNAME" ORDER BY Id DESC LIMIT %s  """
 
-        sql_select_query_3 = """select logtime as lc from public."BitExenLiveDemo" ORDER BY Id DESC LIMIT %s  """
+        sql_select_query_3 = """select logtime as lc from public."DBNAME" ORDER BY Id DESC LIMIT %s  """
         
         cursor.execute(sql_select_query_1, (limitParameter,))
         record_list_1 = list(cursor.fetchall())
